44254e3ac29ebcf4608e31ecbce62a71ae74aa21
Xibitol <xibitol@pimous.dev>
2026-01-17 15:55:30 +0100

    Added docker scripts to launch a caddy dev server.

---
+++ bin/docker/config.sh
@@ -0,0 +1,6 @@
+export DOCKER_FILE=$SCRIPT_DIR/Dockerfile
+
+export DOCKER_IMAGE_REFERENCE=pwg/ws:2.10.0-alpine
+export DOCKER_CONTAINER_NAME=pwg-ws
+
+export SUBPROJECT_DEV_OUTPUT_DIR=$PWD/out/pwg-ws/dev
